33c3337a862ee8d6fafdcbbc2307f2f910eda57f
Nazım ÖZTÜRK <Nazm.OZTURK@hotmail.com.tr>
2022-12-18 17:13:49 +0300

    18.12 API V1

--- client/src/Components/FeatureProducts/FeatureProducts.jsx
+++ client/src/Components/FeatureProducts/FeatureProducts.jsx
@@ -1,45 +1,33 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import Card from "../Card/Card";
 import "./FeatureProducts.scss";
+import axios from "axios";
 
 const FeatureProducts = ({ type }) => {
-  const data = [
-    {
-      id: 1,
-      img: "https://images.pexels.com/photos/8218379/pexels-photo-8218379.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      img2: "https://images.pexels.com/photos/8218381/pexels-photo-8218381.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      title: "Hat",
-      isNew: true,
-      oldPrice: 19,
-      price: 12,
-    },
-    {
-      id: 2,
-      img: "https://images.pexels.com/photos/1381553/pexels-photo-1381553.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      img2: "https://images.pexels.com/photos/1381556/pexels-photo-1381556.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      title: "Coat",
-      isNew: true,
-      oldPrice: 35,
-      price: 30,
-    },
-    {
-      id: 3,
-      img: "https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      title: "Kol Saati",
-      isNew: false,
-      oldPrice: 42,
-      price: 15,
-    },
-    {
-      id: 4,
-      img: "https://images.pexels.com/photos/1928078/pexels-photo-1928078.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      title: "Kapüşonlu Sweatshirt",
-      isNew: false,
-      oldPrice: 10,
-      price: 32,
-    },
-  ];
+  const [data, setData] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          process.env.REACT_APP_API_URL + "/products?populate=*",
+          {
+            headers: {
+              Authorization: "bearer " + process.env.REACT_APP_API_TOKEN,
+            },
+          }
+        );
+        setData(res.data.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
+
+  console.log(data);
   return (
     <>
       <div className="featureProducts">
