c1cf337c124f69f4be081f9a5b18afbd90f8a040
Nazım ÖZTÜRK <Nazm.OZTURK@hotmail.com.tr>
2022-12-22 19:01:41 +0300

    2212

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -33,8 +33,12 @@ const Card = ({ item }) => {
           </div>
           <h2>{item?.attributes.title}</h2>
           <div className="prices">
+            <h3>
+              ₺
+              {item?.attributes.oldPrice ||
+                item?.attributes.price + item?.attributes.price * 0.75}
+            </h3>
             <h3>₺{item?.attributes.price}</h3>
-            <h3>₺{item?.attributes.oldPrice || item?.attributes.price + 20}</h3>
           </div>
         </div>
       </Link>
